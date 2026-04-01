18b98d62716249fcd6e80a3becd3fe71340a7a25
Vladimir Zakharchenko <vladimirzax@mail.ru>
2026-04-01 18:44:29 +0300

    feat: add global styles with CSS variables and Style Guide page with colors, typography, spacing, shadows, and border radius demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { StyleGuide } from "./pages/StyleGuide";
+import './styles/global.scss';
 
 function App() {
 
   return (
-    <>
-    </>
+    <StyleGuide />
   )
 }
 
