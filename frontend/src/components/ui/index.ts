8b6001a0f9f1c752fcad4ce996b4b2020b8c5ca0
Vladimir Zakharchenko <vladimirzax@mail.ru>
2026-04-08 23:19:02 +0300

    refactor: move core UI components to ui/ folder

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,20 @@
+export { Button } from "./Button";
+export type { ButtonProps, ButtonVariant, ButtonSize } from "./Button";
+
+export { Input } from "./Input";
+export type { InputProps } from "./Input";
+
+export { Checkbox } from "./Checkbox";
+export type { CheckboxProps } from "./Checkbox";
+
+export { Radio } from "./Radio";
+export type { RadioProps } from "./Radio";
+
+export { Switch } from "./Switch";
+export type { SwitchProps } from "./Switch";
+
+export { Select } from "./Select";
+export type { SelectProps, SelectOption } from "./Select";
+
+export { Textarea } from "./Textarea";
+export type { TextareaProps } from "./Textarea";
